004f613809be1a93d2f2870103d84f18433403f6
PabloCapilla <pacapilla@gmail.com>
2026-05-04 15:40:04 +0200

    Updated_analysis_260504

---
+++ 02_scripts/01_data_prep/01a_OS_initial_data_preparation_hibernation.R
@@ -0,0 +1,113 @@
+###
+###
+#' Script to prepare data for hibernation meta-analysis (hibernation effect size)
+#'
+###
+###
+
+# Clear memory to make sure there are not files loaded that could cause problems
+rm(list = ls())
+
+
+##
+##### libraries #####
+##
+
+library(openxlsx)
+library(metafor)
+library(tidyverse)
+library(ggokabeito)
+
+##
+##### data #####
+##
+
+##
+## study specific data
+##
+data <- read.xlsx(
+  "./01_data/raw_data/Hibernation_dataset_4_3_hibernation.xlsx",
+  colNames = T,
+  sheet = 1
+)
+head(data)
+summary(data)
+
+#####
+
+##
+##### Descriptive summary of final data and checks to catch errors, typos, etc #####
+##
+summary(data$Hib_M)
+summary(data$Hib_SD)
+
+summary(data$Euthermia_M)
+summary(data$Euthermia_SD)
+
+table(data$Class)
+table(data$Class_2)
+table(data$Thermoregulation)
+table(data$Biomarker_cat_2)
+
+#####
+
+##
+##
+##### Complete SD if missing based on SE #####
+##
+##
+data$Euthermia_SD <- ifelse(
+  is.na(data$Euthermia_SD),
+  data$Euthermia_SE * sqrt(data$Euthermia_N),
+  data$Euthermia_SD
+)
+
+data$Hib_SD <- ifelse(
+  is.na(data$Hib_SD),
+  data$Hib_SE * sqrt(data$Hib_N),
+  data$Hib_SD
+)
+
+####
+
+##
+##### Calculating meta-analysis y variables #####
+##
+
+## SMD
+data <- as.data.frame(escalc(
+  measure = "SMDH",
+  n1i = Hib_N,
+  m1i = Hib_M,
+  sd1i = Hib_SD,
+
+  n2i = Euthermia_N,
+  m2i = Euthermia_M,
+  sd2i = Euthermia_SD,
+
+  data = data,
+  var.names = c("SMDH", "SMDH.sv"),
+  add.measure = F,
+  append = TRUE
+))
+
+
+## checking very high SMD values
+data %>%
+  arrange(desc(SMDH)) %>%
+  head()
+
+## list of papers to check sd values - checked by PB
+df_check <- data %>%
+  filter(!is.na(SMDH)) %>%
+  arrange(desc(SMDH)) %>%
+  filter(SMDH < -4 | SMDH > 4)
+
+##
+##
+##### Save full table #####
+saveRDS(
+  object = data |> filter(!is.na(SMDH)),
+  file = "./01_data/processed_RDS_data_files/01_OS_metaanalysis_hibernation_full_data.RDS"
+)
+#####
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# meta_analysis_hibernation
+# Data and code repository for:
+
+**Effects of animal dormancy on oxidative stress, immune status, and glucocorticoids: a meta-analysis**
+
+Pablo Burraco, Tamara G Petrović, Pablo Capilla-Lasheras, Marko Prokić. [Link to bioRxiv](https://www.biorxiv.org/content/10.1101/2025.10.02.680078v1.full.pdf)
+
+# Repositoty structure:
+
+- **01_data**:
+
+  - *processed_RDS_data_files*: RDS files produced by [prep'ing script](LINK). These are the data files used in subsequent analyses.
+
+  - *raw_data*: raw datatables to start analysis.
+
+- **02_scripts**:
+
+  - *01_data_prep*: scripts to process (raw data)\[LINK\].
+
+  - *02_phylo_cor*: scripts to generate phylogenetic var-covar matrices for phylogenetically-controlled meta-analysis.
+
+  - *03_analysis*: R markdown documents with all models and plots.
+
+- **03_plots**:
+
+  - *01_hibernation*
+
+  - *02_arousal*
+
+  - *03_aestivation*
+
+- **04_models**:
+
+  - *01_hibernation*
+
+  - *02_arousal*
+
+  - *03_aestivation*
+
+- **05_reports**: html files showing content of each (analysis script)\[LINK\].
